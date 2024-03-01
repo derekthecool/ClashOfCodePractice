b9c2d7b54419429f2edc31c431b12e05813efe5d
Derek Lomax <derekthecool@gmail.com>
2024-03-01 15:05:24 -0700

    Add more code!

--- c/clash.c
+++ c/clash.c
@@ -1,72 +1,11 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
-main()
+int main()
 {
-    char input[999];
-    scanf("%s", input);
-
-    char *p, *l = 0;
-    getdelim(&p, &l, 0xFF, stdin);
-
-    int angle = 0;
-    bool toggle = false;
-
-    char separator[] = " \n";
-    for (char *string_to_split = strtok(p, separator); string_to_split != NULL;
-         string_to_split = strtok(NULL, separator))
-    {
-         if(toggle)
-         {
-             toggle = false;
-         }
-         else
-         {
-             toggle = true;
-         }
-
-        angle += atoi(string_to_split);
-        switch (angle)
-        {
-        case 90:
-            puts("SUPPLEMENTARY");
-            break;
-        case 180:
-            puts("COMPLEMENTARY");
-            break;
-        default:
-            puts("NEITHER");
-            break;
-        }
-    }
-
-    for(int i = 0; i < 10; i++)
-    {
-        
-    }
-
-    // printf("%s",p);
-}
-
-for(int i = 0; i < 10; i++)
-{
-    
-}
-
-if()
-{
-    
-}
-
-else
-{
-    
-    else if()
-    {
-        int MyFunction()
-        {
-            
-        }
-    }
+    char S[101];
+    scanf("%[^\nA-Za-z]", S);
+    printf("%d",strlen(S));
 }
--- dart/clash.dart
+++ dart/clash.dart
@@ -1,10 +1,17 @@
 import 'dart:io';
-
-String readLineSync() {
-  String? s = stdin.readLineSync();
-  return s == null ? '' : s;
-}
-
 void main() {
-  if (true) {}
+var n=int.parse(stdin.readLineSync()!);
+var a=print;
+if(n<32){a('January');}
+else if(n<61){a('February');}
+else if(n<92){a('March');}
+else if(n<122){a('April');}
+else if(n<153){a('May');}
+else if(n<183){a('June');}
+else if(n<213){a('July');}
+else if(n<244){a('August');}
+else if(n<274){a('September');}
+else if(n<305){a('October');}
+else if(n<335){a('November');}
+else {a('December');}
 }
