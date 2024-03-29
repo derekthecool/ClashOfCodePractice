bf913a5c4ff95104c6da003e53e2d9303ef3f142
Derek Lomax <derekthecool@gmail.com>
2024-03-29 15:03:44 -0600

    First real example with multiple solutions for the lua multisolution script

--- lua/multi-solution.lua
+++ lua/multi-solution.lua
@@ -1,53 +1,34 @@
 -- Create any number of solutions here to test the code
 local function testSolutions(input)
+    io.stderr:write(string.format('input:\n%s\n', input))
     local solutions = {
-        basicSolution = function(x)
-            local product, sum = 1, 0
-            for i = 1, x do
-                product = product * i
-                sum = sum + i
+        function(x)
+            local S = 0
+            local b
+            for a in x:gmatch('.') do
+                if not b then
+                    b = a
+                elseif a ~= b then
+                    S = S + 1
+                    b = a
+                end
             end
-            return product + sum
+            return S
         end,
 
-        -- fancyPantsMathSolution = function(x)
-        --     local sum = (x * (x + 1)) / 2
-        --
-        --     -- Boo-hoo there is no awesome formula without looping or recursion
-        --     -- for factorials. Use this instead
-        --     local factorialTable = {
-        --         1, -- 0!
-        --         1, -- 1!
-        --         2, -- 2!
-        --         6, -- 3!
-        --         24, -- 4!
-        --         120, -- 5!
-        --         720, -- 6!
-        --         5040, -- 7!
-        --         40320, -- 8!
-        --         362880, -- 9!
-        --         3628800, -- 10!
-        --         39916800, -- 11!
-        --         479001600, -- 12!
-        --         6227020800, -- 13!
-        --         87178291200, -- 14!
-        --         1307674368000, -- 15!
-        --         20922789888000, -- 16!
-        --         355687428096000, -- 17!
-        --         6402373705728000, -- 18!
-        --         121645100408832000, -- 19!
-        --         2432902008176640000, -- 20!
-        --         51090942171709440000, -- 21!
-        --         1124000727777607680000, -- 22!
-        --         25852016738884976640000, -- 23!
-        --         620448401733239439360000, -- 24!
-        --         15511210043330985984000000, -- 25!
-        --     }
-        --
-        --     local product = factorialTable[x]
-        --
-        --     return sum + product
-        -- end,
+        function(x)
+            local changes = 0
+            local lastDigit = x:sub(1, 1)
+
+            for digit in x:gmatch('(%d)') do
+                if digit ~= lastDigit then
+                    changes = changes + 1
+                    lastDigit = digit
+                end
+            end
+
+            return changes
+        end,
     }
 
     -- Variable to store the expected result (initially nil)
